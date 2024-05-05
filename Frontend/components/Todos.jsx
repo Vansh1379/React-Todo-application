ace2b22e67da9f2a502e3a3472caee227838493b
vansh kalra <vanshkalra1379@gmail.com>
2024-05-06 01:19:42 +0530

    Frontend

--- Frontend/components/Todos.jsx
+++ Frontend/components/Todos.jsx
@@ -1,10 +1,24 @@
-
-export function Todos() {
+/*
+    where todos is array 
+    todos = [
+        title : 
+        description: 
+    ]
+  */
+export function Todos({ todos }) {
     return (
-        <div>
-            <h3>Go To GYM</h3>
-            <h3>Go GYM</h3>
-            <button>Mask As Completed</button>
-        </div>
+        <>
+            {
+                todos.map(function (todo) {
+                    return (
+                        <div>
+                            <h1>{todo.title}</h1>
+                            <h3>{todo.description}</h3>
+                            <button>{todo.complete == true ? "Completed" : "Mark as Completed"}</button>
+                        </div>
+                    );
+                })
+            }
+        </>
     );
 }
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,13 +6,20 @@ import { CreateTodo } from '../components/CreateTodo'
 import { Todos } from '../components/Todos'
 
 function App() {
+  // const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
+  fetch("http://localhost:3000/todo")
+    .then(async function (res) {
+      const json = await res.json();
+      setTodos(json.todos);
+    });
   return (
     <div>
       <CreateTodo />
-      <Todos />
+      <Todos todos={todos} />
     </div>
-  )
+  );
 }
 
 export default App
