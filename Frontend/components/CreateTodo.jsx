ed5f96ebc19e961d6a07ae89093aad4bc707fb24
vansh kalra <vanshkalra1379@gmail.com>
2024-05-06 00:08:21 +0530

    Frontend

---
+++ Frontend/components/CreateTodo.jsx
@@ -0,0 +1,15 @@
+
+export function CreateTodo() {
+    return (
+        <div>
+            <h1>TODO APP</h1>
+            <input type="text" placeholder="Title" />
+            <br />
+            <br />
+            <input type="text" placeholder="Description" />
+            <br />
+            <br />
+            <button>Add a Todo</button>
+        </div>
+    );
+}
---
+++ Frontend/components/Todos.jsx
@@ -0,0 +1,10 @@
+
+export function Todos() {
+    return (
+        <div>
+            <h3>Go To GYM</h3>
+            <h3>Go GYM</h3>
+            <button>Mask As Completed</button>
+        </div>
+    );
+}
