0ab05afd1d9b382ad47b039fe29d55c51fd90f19
vansh kalra <vanshkalra1379@gmail.com>
2024-07-12 19:29:00 +0530

    Backend

--- Todo Application/Backend/schema/db.js
+++ Todo Application/Backend/schema/db.js
@@ -5,7 +5,10 @@ mongoose.connect('mongodb+srv://vanshkalra1379:kalra1379@cluster0.rjd2jaw.mongod
 const todoSchema = new mongoose.Schema({
     title: String,
     description: String,
-    completed: Boolean,
+    completed: {
+        type: Boolean,
+        default: false,
+    }
 });
 
 const Todo = mongoose.model('todos', todoSchema);
